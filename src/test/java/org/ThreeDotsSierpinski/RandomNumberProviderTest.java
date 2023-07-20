b7a9de6961f32ea0ef3a476f2697b93b6587d075
Your Name <you@example.com>
2023-07-20 09:49:36 +0200

    test

--- src/test/java/org/ThreeDotsSierpinski/RandomNumberProviderTest.java
+++ src/test/java/org/ThreeDotsSierpinski/RandomNumberProviderTest.java
@@ -2,10 +2,8 @@ package org.ThreeDotsSierpinski;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RandomNumberProviderTest {
 
