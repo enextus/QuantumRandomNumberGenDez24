7105c265caa099b2a82ec2ed432a9715762420bb
Your Name <you@example.com>
2024-07-05 03:27:49 +0200

    115. commit

--- src/main/java/org/ThreeDotsSierpinski/App.java
+++ src/main/java/org/ThreeDotsSierpinski/App.java
@@ -10,29 +10,20 @@ public class App {
     public static final int DELAY = 0; // 5000 for slow
 
     public static void main(String[] args) {
-
         DotController dotController = new DotController();
-
         SwingUtilities.invokeLater(() -> {
-
             JFrame frame = new JFrame(DOT_MOVER);
             RandomNumberProvider randomNumberProvider = new RandomNumberProvider();
-
             frame.add(dotController);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
             Timer timer = new Timer(DELAY, e -> {
-
                 dotController.moveDot();
-
                 // Get a random number value from RandomNumberProvider
                 int randomValue = randomNumberProvider.getNextRandomNumber();
-
                 // Update the window title with the dot counter readings and the value of the random number
                 frame.setTitle(String.format("%s%d%s%d%s", DOT_MOVER_DOTS, dotController.getDotCounter(), RANDOM_VALUE_STRING, randomValue, CLOSING_PARENTHESIS));
             });
-
             timer.start();
             frame.setVisible(true);
         });
